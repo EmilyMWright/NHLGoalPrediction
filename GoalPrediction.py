38542f801f1587ec1339c219679e81ea5ae418fe
Emily Wright <emily.wright.mi@gmail.com>
2020-06-24 22:27:14 -0700

    Finds average time between goal and last faceoff prior to goal

--- GoalPrediction.py
+++ GoalPrediction.py
@@ -1,6 +1,7 @@
 import pandas as pd
-import numpy as np
+import statistics as stats
 
+# Loads and cleans data
 event_headers = ['play_id', 'game_id', 'team_id_for', 'team_id_against', 'event', 'st_x', 'st_y', 'period', 'periodTime']
 event_df = pd.read_csv('game_plays.csv', usecols = event_headers)
 
@@ -18,8 +19,13 @@ team_id_dict = {1:'NJD', 2:'NYI', 3:'NYR', 4:'PHI', 5:'PIT', \
 event_df.team_id_for.replace(team_id_dict, inplace=True)
 event_df.team_id_against.replace(team_id_dict, inplace=True)
 
+# Finds average time between goal and last faceoff prior to goal
 goal_df = event_df[event_df.event.isin(['Goal'])].reset_index()
 goal_foff_df = event_df[event_df.event.isin(['Goal', 'Faceoff'])].reset_index()
 foff_indices = goal_foff_df[goal_foff_df.event == 'Goal'].index - 1
 
-goal_foff_df = goal_foff_df.iloc[foff_indices]
+last_foff_df = goal_foff_df.iloc[foff_indices]
+
+warning_time = stats.mean(goal_df.periodTime.values - last_foff_df.periodTime.values)
+
+print(warning_time)
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # NHL Goal Prediction
 
-Predicting goals in event sequences using Kaggle NHL Game Data
+## Predicting goals in event sequences using Kaggle NHL Game Data.
+
+The average time between goal and last faceoff prior to goal is 55 seconds.
