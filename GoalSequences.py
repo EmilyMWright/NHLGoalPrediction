70c2e6fe4200917393344bf6eca2f0ef3fe280fb
Emily Wright <emily.wright.mi@gmail.com>
2020-06-28 21:47:58 -0700

    Writes lead-up sequences to csv

--- GoalSequences.py
+++ GoalSequences.py
@@ -2,7 +2,7 @@ import pandas as pd
 import csv
 
 # Loads and cleans data
-event_headers = ['event', 'periodTime', 'Team']
+event_headers = ['event', 'periodTime', 'team_id_for']
 event_df = pd.read_csv('game_plays.csv', usecols = event_headers)
 
 events = ['Faceoff', 'Giveaway', 'Blocked Shot', 'Shot', 'Hit', 'Goal', 'Penalty', 'Takeaway', 'Missed Shot']
@@ -16,21 +16,21 @@ teams = goal_df.team_id_for
 with open('goal_sequences.csv', 'w', newline='') as f:
     writer = csv.writer(f)
 
-    for j in len(goal_df):
+    for j in range(len(goal_df)):
         i = indices[j]
         t = times[j]
         team = teams[j]
-        e = event_df.iloc[i]
         sequence = []
         if t > 55:
             min_t = t - 55
         else:
             min_t = 0
-        while (e.periodTime > min_t):
-            if e.team_id_for == team:
-                sequence.append('O' + e.event.value)
+        while (event_df.iloc[i].periodTime > min_t):
+            if event_df.iloc[i].team_id_for == team:
+                sequence.append('O' + event_df.iloc[i].event)
             else:
-                sequence.append('D' + e.event.value)
+                sequence.append('D' + event_df.iloc[i].event)
+            i = i - 1
         writer.writerow(sequence)
     
 
