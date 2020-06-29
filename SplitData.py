c9bb15d4fa65a1ee776587ffc7c733479299fbfd
Emily Wright <emily.wright.mi@gmail.com>
2020-06-28 21:06:42 -0700

    Splits game data intro train and eval and writes to csv

--- SplitData.py
+++ SplitData.py
@@ -1,3 +1,11 @@
 import pandas as pd
 
-event_df = pd.read_csv('game_plays.csv', usecols = event_headers)
+event_df = pd.read_csv('game_plays.csv')
+
+train_ids = event_df.game_id.sample(n=9000)
+
+train_df = event_df[event_df.game_id.isin(train_ids)].reset_index()
+eval_df = event_df[~event_df.game_id.isin(train_ids)].reset_index()
+
+train_df.to_csv('train_plays.csv')
+eval_df.to_csv('eval_plays.csv')
