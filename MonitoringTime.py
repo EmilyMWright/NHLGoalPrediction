177b9e18ba8256920f5b7e726c2cd35016b4b169
Emily Wright <emily.wright.mi@gmail.com>
2020-06-28 20:33:36 -0700

    Calculates average time between faceoffs

--- MonitoringTime.py
+++ MonitoringTime.py
@@ -2,20 +2,12 @@ import pandas as pd
 import statistics as stats
 
 # Loads and cleans data
-event_headers = ['game_id', 'event', 'period', 'periodTime']
+event_headers = ['event', 'periodType']
 event_df = pd.read_csv('game_plays.csv', usecols = event_headers)
 
-foff_df = event_df[event_df.event == 'Faceoff'].reset_index()
+num_games = (event_df.event == 'Game Scheduled').sum()
+num_foffs = ((event_df.event == 'Faceoff') & (event_df.periodType == 'REGULAR')).sum()
 
-periods = [pd.DataFrame(y) for x, y in foff_df.groupby(['game_id', 'period'], as_index=False)]
+monitoring_time = num_games*60/num_foffs
 
-# Finds average time between faceoffs
-print(stats.mean(periods[0].periodTime.values[1:] - periods[0].periodTime.values[:-1]))
-
-#means = []
-#for period in periods:
-    #means.append(stats.mean(period.periodTime.values[1:] - period.periodTime.values[:-1]))
-
-#monitoring_time = stats.mean(means)
-
-#print(monitoring_time)
+print(monitoring_time)
