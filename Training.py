531a7b298cab0f14539c670afa5ecd5fddfbf566
Emily Wright <emily.wright.mi@gmail.com>
2020-06-29 21:58:32 -0700

    Counts average sequence length

--- Training.py
+++ Training.py
@@ -1,1 +1,8 @@
-print("Hello world")
+import pandas as pd
+
+headers = []
+for i in range(12):
+    headers.append("Play" + str(i + 1))
+
+leadup_df = pd.read_csv('goal_sequences.csv', names = headers)
+print(leadup_df.count(axis='columns').mean())
