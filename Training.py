7013657b0ca79ede81c1503934eae6354b6a1947
Emily Wright <emily.wright.mi@gmail.com>
2020-06-30 16:36:35 -0700

    Saves all 3 play sequences to csv

--- Training.py
+++ Training.py
@@ -1,9 +1,21 @@
 import pandas as pd
+import csv
 
 headers = []
-for i in range(12):
+for i in range(24):
     headers.append("Play" + str(i + 1))
 
 leadup_df = pd.read_csv('goal_sequences.csv', names = headers)
-count_df = leadup_df.count(axis='columns')
-print(100*count_df[count_df >= 3].count()/count_df.count())
+leadup_df = leadup_df[leadup_df.count(axis='columns') >= 3]
+
+with open('three_play_sequences.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+
+    for index, seq in leadup_df.iterrows():
+        for i in range(seq.count() - 2):
+            three_plays = []
+            three_plays.append(seq["Play" + str(i + 1)])
+            three_plays.append(seq["Play" + str(i + 2)])
+            three_plays.append(seq["Play" + str(i + 3)])
+            writer.writerow(three_plays)
+    
