bdaa2951bf1dbaa8268022653e992d83e0a3ea7a
Emily Wright <emily.wright.mi@gmail.com>
2020-06-29 21:31:35 -0700

    Changes time period for lead-up sequences'

--- GoalSequences.py
+++ GoalSequences.py
@@ -3,12 +3,14 @@ import csv
 
 # Loads and cleans data
 event_headers = ['event', 'periodTime', 'team_id_for']
-event_df = pd.read_csv('game_plays.csv', usecols = event_headers)
+event_df = pd.read_csv('train_plays.csv', usecols = event_headers)
 
 events = ['Faceoff', 'Giveaway', 'Blocked Shot', 'Shot', 'Hit', 'Goal', 'Penalty', 'Takeaway', 'Missed Shot']
 
 event_df = event_df[event_df.event.isin(events)].reset_index()
 goal_df = event_df[event_df.event == 'Goal'].reset_index()
+
+# saves goal lead-up sequences to csv
 indices = goal_df.level_0
 times = goal_df.periodTime
 teams = goal_df.team_id_for
@@ -22,15 +24,19 @@ with open('goal_sequences.csv', 'w', newline='') as f:
         team = teams[j]
         sequence = []
         if t > 55:
-            min_t = t - 55
-        else:
-            min_t = 0
-        while (event_df.iloc[i].periodTime > min_t):
-            if event_df.iloc[i].team_id_for == team:
-                sequence.append('O' + event_df.iloc[i].event)
+            max_t = t - 55
+            if t > 115:
+                min_t = t - 115
             else:
-                sequence.append('D' + event_df.iloc[i].event)
-            i = i - 1
-        writer.writerow(sequence)
+                min_t = 0
+            while (event_df.iloc[i].periodTime > max_t):
+                i = i - 1
+            while (event_df.iloc[i].periodTime > min_t):
+                if event_df.iloc[i].team_id_for == team:
+                    sequence.append('O' + event_df.iloc[i].event)
+                else:
+                    sequence.append('D' + event_df.iloc[i].event)
+                i = i - 1
+            writer.writerow(sequence)
     
 
