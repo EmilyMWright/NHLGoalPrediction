da45ac21a4c28eb9d254de63b12baa921dcbc3e8
Emily Wright <emily.wright.mi@gmail.com>
2020-06-30 19:09:05 -0700

    Records unqiue sequences and their counts

--- TrainingFrequency.py
+++ TrainingFrequency.py
@@ -1,3 +1,10 @@
 import pandas as pd
 
-sequences_df = pd.read_csv('three_play_sequences.csv')
+sequences_df = pd.read_csv('three_play_sequences.csv', names = ['play1', 'play2', 'play3'])
+sequences_df['Level'] = sequences_df.index
+
+sequences_df['Freq'] = sequences_df.groupby(['play1', 'play2', 'play3'])['Level'].transform('count')
+
+sequences_df = sequences_df.drop('Level', axis=1).drop_duplicates()
+
+print(len(sequences_df))
