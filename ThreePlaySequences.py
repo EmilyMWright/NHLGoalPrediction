7eff3f3f9123e3f96dd50658e9204f91305ec576
Emily Wright <emily.wright.mi@gmail.com>
2020-07-02 19:03:41 -0700

    Saves goal_play_ids in which sequences appear

--- ThreePlaySequences.py
+++ ThreePlaySequences.py
@@ -6,13 +6,13 @@ for i in range(25):
     headers.append("Play" + str(i + 1))
 
 leadup_df = pd.read_csv('goal_sequences.csv', names = headers)
-leadup_df = leadup_df[leadup_df.count(axis='columns') >= 3]
+leadup_df = leadup_df[leadup_df.count(axis='columns') >= 4]
 
 with open('three_play_sequences.csv', 'w', newline='') as f:
     writer = csv.writer(f)
 
     for index, seq in leadup_df.iterrows():
-        for i in range(seq.count() - 2):
+        for i in range(seq.count() - 3):
             three_plays = [seq.play_id]
             three_plays.append(seq["Play" + str(i + 1)])
             three_plays.append(seq["Play" + str(i + 2)])
